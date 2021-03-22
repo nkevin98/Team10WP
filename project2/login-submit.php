d78a059311876cb9ccb909949503b151f7e7e3c1
snguyen36 <43546896+snguyen36@users.noreply.github.com>
2021-03-22 15:35:04 -0400

    remove comments

--- project2/login-submit.php
+++ project2/login-submit.php
@@ -1,21 +1,15 @@
 <?php 
     session_save_path("./");
     session_start();
-    /* Check Login form submitted */    
     if(isset($_POST['Submit'])){
-        /* Check and assign submitted Username and Password to new variable */
         $Username = isset($_POST['Username']) ? $_POST['Username'] : '';
-        $Password = isset($_POST['Password']) ? $_POST['Password'] : '';
-        
-        /* Check Username and Password existence in defined array */        
+        $Password = isset($_POST['Password']) ? $_POST['Password'] : '';  
         if (isset($_SESSION['logins'][$Username]) && $_SESSION['logins'][$Username] == $Password){
-            /* Success: Set session variables and redirect to Protected page  */
             $_SESSION['UserData']['Username'] = $_SESSION['logins'][$Username];
             $_SESSION['username'] = $Username;
             header("location:start.php");
             exit;
         } else {
-            /*Unsuccessful attempt: Set error message */
             $_SESSION['msg'] = "<span style='color:red'>Invalid Login Details</span>";
             header("location:login.php");
             exit();
